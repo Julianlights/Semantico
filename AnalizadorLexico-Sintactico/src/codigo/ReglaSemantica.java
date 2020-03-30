7f4405a859f4cdf20591a900465c44d0c8cc792b
MarceloLopezGuzman <173215@ids.upchiapas.edu.mx>
2020-03-29 21:44:40 -0600

    Add test-examples

--- AnalizadorLexico-Sintactico/src/codigo/ReglaSemantica.java
+++ AnalizadorLexico-Sintactico/src/codigo/ReglaSemantica.java
@@ -4,7 +4,52 @@ package codigo;
  *
  * @author Marcelo Lopez
  */
-public class ReglaSemantica {
+public class Test {
     
-        
+    String sstring;
+    int entero_1;
+    private float flot;
+    float testData;
+    double dob;
+
+    // El constructor puede quedar vacío
+    // El constructor NO siempre debe de llevar  los atributos ceclarados
+    // Los nombres de los atributos en el constructor puede ser diferente del nombre de los atributos declarados
+    // El constructor puede ser private o public
+    
+    public Test(String data, int Int, double doble, float testData) {}
+    
+    
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getInt() {
+        return Int;
+    }
+
+    public void setInt(int Int) {
+        this.Int = Int;
+    }
+
+    public double getDoble() {
+        return doble;
+    }
+
+    public void setDoble(double doble) {
+        this.doble = doble;
+    }
+
+    public float getTestData() {
+        return testData;
+    }
+
+    public void setTestData(float testData) {
+        this.testData = testData;
+    }      
 }
